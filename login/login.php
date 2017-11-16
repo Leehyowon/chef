0df64aec07ba31a0081b0a6a14380f0dc0d86d42
남혜민 <namhyemin@namhyemin-ui-MacBook-Air.local>
2017-11-16 20:00:51 +0900

    css

--- login/login.php
+++ login/login.php
@@ -9,10 +9,10 @@
 
     <body>
         <nav>
-            <li><a href = "/chef/login/login.php"> Login </a></li>
-            <li><a href = "/chef/login/signup.php"> SignUp </a></li>
-            <li><a href = ""> Mypage </a></li>
-            <li><a href = ""> Cart </a></li>
+            <li class="login"><a href = "/chef/login/login.php"> Login </a></li>
+            <li class="login"><a href = "/chef/login/signup.php"> SignUp </a></li>
+            <li class="login"><a href = ""> Mypage </a></li>
+            <li class="login"><a href = ""> Cart </a></li>
         </nav>
 
         <main>
@@ -24,36 +24,37 @@
 
                 <div class = "sidebar">
                     <ul>
-                        <li>SHOPPING
+                        <li><button class="btn">SHOPPING</button>
                             <ul>
-                                <li><a href = ""> by BRAND </a></li>
+                                <li><a href = ""> <button class="btn-2">by BRAND</button></a></li>
                                 <ul>
-                                    <li><a href = "">Wootique</a></li>
-                                    <li><a href = "">Dalrang</a></li>
-                                    <li><a href = "">Clothes1</a></li>
-                                    <li><a href = "">Clothes2</a></li>
-                                    <li><a href = "">Slime1</a></li>
-                                    <li><a href = "">Slime2</a></li>
+                                    <li class="mincate"><a href = "">Wootique</a></li>
+                                    <li class="mincate"><a href = "">Dalrang</a></li>
+                                    <li class="mincate"><a href = "">Clothes1</a></li>
+                                    <li class="mincate"><a href = "">Clothes2</a></li>
+                                    <li class="mincate"><a href = "">Slime1</a></li>
+                                    <li class="mincate"><a href = "">Slime2</a></li>
                                 </ul>
-                                <li><a href = ""> by PRODUCT </a></li>
+                                <li><a href = ""> <button class="btn-2">by PRODUCT</button></a></li>
                                 <ul>
-                                    <li><a href = "">Clothes</a></li>
-                                    <li><a href = "">Jewelry</a></li>
-                                    <li><a href = "">Slime</a></li>
+                                    <li class="mincate"><a href = "">Clothes</a></li>
+                                    <li class="mincate"><a href = "">Jewelry</a></li>
+                                    <li class="mincate"><a href = "">Slime</a></li>
                                 </ul>
                             </ul>
                         </li>
-                        <li>COMMUNITY
+                        <li><button class="btn">COMMUNITY</button>
                             <ul>
-                                <li><a href = "/chef/board/notice.html"> Notice </a></li>
-                                <li><a href = "/chef/board/event.html"> Event </a></li>
-                                <li><a href = "/chef/board/qna.html"> QnA </a></li>
-                                <li><a href = "/chef/board/info.html"> Information </a></li>
+                                <li class="mincate"><a href = "/chef/board/notice.html"> Notice </a></li>
+                                <li class="mincate"><a href = "/chef/board/event.html"> Event </a></li>
+                                <li class="mincate"><a href = "/chef/board/qna.html"> QnA </a></li>
+                                <li class="mincate"><a href = "/chef/board/info.html"> Information </a></li>
                             </ul>
                         </li>
                     </ul>
                 </div>
             </aside>
+
 
             <article>
                 <div class = "slide">
@@ -125,39 +126,35 @@
         </main>
 
         <footer>
-            <div class = "phone">
-                <div class = "footer_title"> CS center </div>
-                <div id = "num">
-                    1588-1588
-                </div>
-                <div id = "operating_hour">
-                    상담시간 : 09:00 ~ 17:00 </br>
-                    점심시간 : 12:00 ~ 13:00
-                </div>
-            </div>
+        <div class = "phone">
+            CS center
+            <ul>
+                <li>1588-1588</li>
+                <li>상담시간 : 09:00 ~ 17:00</li>
+                <li>점심시간 : 12:00 ~ 13:00 </li>
+            </ul>         
+        </div>
 
-            <div class = "bank">
-                <div class = "footer_title"> BANK info </div>
-                <div id = "account">
-                    신한 110386389614 (주)셰프 </br>
-                    기업 1111222233333 (주)셰프 </br>
-                    국민 245656565555 (주)셰프
-                </div>
-            </div>
+        <div class = "bank">
+            BANK info   
+            <ul>
+                <li>신한 110386389614 (주)셰프</li>
+                <li>기업 1111222233333 (주)셰프</li>
+                <li>국민 245656565555 (주)셰프</li>
+            </ul>                      
+        </div>
 
-            <div class = "company">
-                <div class = "footer_title"> 기업정보 </div>
-                <div id = "com_info">
-                    <ul>
-                        <li>회사 : (주)셰프</li>
-                        <li>대표 : 김웹앱</li>
-                        <li>주소 : 경기도 안산시 상록구 사동 1271 한양대학교 제4공학관 SMaSH룸</li>
-                        <li>사업자등록번호 : [101-11-22222]</li>
-                        <li>통신판매업신고 : 제2017-경기안산-0285호 [ 사업자정보확인 ]</li>
-                        <li>제휴/제안 : info@hanyang.ac.kr</li>
-                    </ul>
-                </div>
-            </div>
-        </footer>
+        <div class = "company">
+            기업정보                    
+            <ul>
+                <li>회사 : (주)셰프</li>
+                <li>대표 : 김웹앱</li>
+                <li>주소 : 경기도 안산시 상록구 사동 1271 한양대학교 제4공학관 SMaSH룸</li>
+                <li>사업자등록번호 : [101-11-22222]</li>
+                <li>통신판매업신고 : 제2017-경기안산-0285호 [ 사업자정보확인 ]</li>
+                <li>제휴/제안 : info@hanyang.ac.kr</li>
+            </ul>                
+        </div>
+    </footer>
     </body>
 </html>
