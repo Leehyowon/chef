210b13ae7c86bd5e73ba336d1fd1a0c3718ee7eb
chaeeun <chaeeun@chaeeunui-MacBook-Air.local>
2017-11-24 13:20:47 +0900

    msql

--- login/chef.sql
+++ login/chef.sql
@@ -35,6 +35,9 @@ CREATE TABLE `buy`(
 
 INSERT INTO `product` VALUES (1, 'wootique', 8000, 100, 1,2017-10-11);
 
-INSERT INTO `consumer` VALUES ('abc1001','def0110','abc1001@naver.com','Jina','01012345678',1998-02-13,'F');
 
-INSERT INTO `buy` VALUES (0001,1,'abc1001','Ansan hanyang university',2017-10-29,'credit');
+INSERT INTO `consumer` VALUES ('abc1001','def0110','abc1001@naver.com','Jina','01012345678','1998-02-13','F');
+
+
+INSERT INTO `buy` VALUES (0001,1,'abc1001','Ansan hanyang university','2017-10-29','credit');
+
