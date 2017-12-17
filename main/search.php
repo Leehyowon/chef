ad539888567cdbfe7478fcc3ec9a1c893d0e919e
ToasT1ng <jrainboww9@gmail.com>
2017-12-17 17:34:14 +0900

    search

--- main/search.php
+++ main/search.php
@@ -5,6 +5,7 @@
 		<title>CHEF</title>
 		<link href="/chef/main/favicon.png" type="image/png" rel="shortcut icon"/>
         <link href="/chef/main/main.css" type="text/css" rel="stylesheet" />
+        <link href="/chef/main/search.css" type="text/css" rel="stylesheet" />
         <script src="/chef/main/search.js"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js" type="text/javascript"></script>
     </head>
@@ -60,11 +61,13 @@
                     <br/>
                     <br/>
                     <br/>
-                    <p>요기</p> <!--here-->
+                    <p>요기</p> <!-- search 하는 부분 with submit 버튼 -->
                     <form method="post">
                         <input type="text" name="searchName" id="search" placeholder="search" />
                         <input type="submit" name="search" value="search" />
                     </form>
+                    <!-- 여기까지 -->
+
                     <?php
                         $host = 'localhost';
                         $user = 'chef';
@@ -79,37 +82,44 @@
                             while($row = $result->fetch_assoc()){
                                 $all[] = $row["name"];
                             }
-                            // print_r($row);
-                        } else {
-                           
-                        }
+                        } 
                     ?>
 
-                    <div class="my">
+                    <!-- 전체리스트! js로 처음에 안보이게 해둠. 값을 입력하면 일치하는 친구들 밑에 나오게 만듬. -->
+                    <div>
                         <ul id="myUL">
                             <?php 
                             foreach ($all as $value) {
                             ?>
-                                <li><a><?=$value?></a></li>
+                                <li><a href="#"><?=$value?></a></li>
                             <?php
                             }
                             ?>
                         </ul>
                     </div>
+                    <!-- 여기까지 -->
 
                     <?php
                         if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             $name = $_POST['searchName'];
                         }
 
-                        $sql = "SELECT name FROM product WHERE name='".$name."'";
+                        $sql = "SELECT name FROM product WHERE name like '%".$name."%'";
                         $result = $mysqli->query($sql);
                         $info = array();
                         if ($result->num_rows > 0) {
                             while($row = $result->fetch_assoc()){
                                 $info[] = $row["name"];
                             }
-                            print_r($info);
+                            foreach ($info as $inform) {
+                    ?>
+
+                            <!-- 검색(enter)한 후 나오는 결과값 -->
+                            <li><a href="#"><?=$inform?></a></li>
+                            <!-- 여기까지 -->
+
+                    <?php
+                            }
                         } else {
                             print "검색결과 없음";
                         }
